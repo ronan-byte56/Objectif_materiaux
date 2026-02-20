8774f1a95502c44a24931e923ad712dd291107c6
ronan-byte56 <ronanlebailmail@gmail.com>
2026-02-20 23:47:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+{
+  "name": "Objectif Matériaux",
+  "short_name": "ObjMatos",
+  "description": "Quiz pédagogique sur les matériaux",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#3498db",
+  "icons": [
+    {
+      "src": "logo.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
